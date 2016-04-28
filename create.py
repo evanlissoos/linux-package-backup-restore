00a1b4f61a78f276a6a4a08c39107184487d87aa
Fedora Cloud User <fedora@evan-droplet.os.ncsa.edu>
2016-04-28 23:21:04 +0000

    testing

--- create.py
+++ create.py
@@ -65,7 +65,7 @@ ID_FILE.write(UNIQUE_ID + "\n")
 
 #Git sequence for commiting ids.txt file changes
 #os.system("git add .snap/ids.txt")
-os.system('git commit -am .snap "Commiting ID file updates"')
+os.system('git commit -am "Commiting ID file updates"')
 
 print "Your unique ID is " + UNIQUE_ID
 
